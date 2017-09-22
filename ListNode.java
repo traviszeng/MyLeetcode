a7893b456b2af0bbab700e90e1fdd0623c90ab05
travis <traviszeng7@gmail.com>
2017-09-22 17:42:13 +0800

    finish problem 83 and still need to solve problem 437

--- ListNode.java
+++ ListNode.java
@@ -1,10 +1,56 @@
 /**
- * Created by Administrator on 2017/1/19.
+ * Created by Travis Zeng on 2017/1/19.
  */
 public class ListNode {
     int val;
     ListNode next;
     ListNode(int x){
         val=x;
+    }
+
+
+    /**
+     *  Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+     For example,
+     Given 1->1->2, return 1->2.
+     Given 1->1->2->3->3, return 1->2->3.
+     * */
+    public ListNode deteleDuplicates(ListNode head){
+        if(head==null) return null;
+        while(head.next!=null && head.val == head.next.val){
+            head.next = head.next.next;
+        }
+        if(head.next!=null&& head.val!=head.next.val) {
+            head.next = deteleDuplicates(head.next);
+        }
+
+        return head;
+
+    }
+
+    /**
+     * traverse function of LinkList
+     * print the LinkedList as 1 2 3
+     * */
+    public void walkThroughLinkedList(ListNode head){
+        if(head==null) return;
+        while(head!=null){
+            System.out.print(head.val+" ");
+            head = head.next;
+        }
+    }
+
+
+    public static void main(String[] args){
+        ListNode s= new ListNode(1);
+        s.next = new ListNode(1);
+        s.next.next = new ListNode(1);
+        s.next.next.next = new ListNode(2);
+        s.next.next.next.next = new ListNode(2);
+        s.next.next.next.next.next = new ListNode(3);
+        ListNode ss = s.deteleDuplicates(s);
+        ss.walkThroughLinkedList(ss);
+
     }
 }
--- TreeNode.java
+++ TreeNode.java
@@ -434,6 +434,41 @@ public class TreeNode {
     }
 
 
+    /**
+     * Path sum III
+     * You are given a binary tree in which each node contains an integer value.
+
+     Find the number of paths that sum to a given value.
+
+     The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+     The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+     Example:
+
+     root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+     10
+     /  \
+     5   -3
+     / \    \
+     3   2   11
+     / \   \
+     3  -2   1
+
+     Return 3. The paths that sum to 8 are:
+
+     1.  5 -> 3
+     2.  5 -> 2 -> 1
+     3. -3 -> 11
+
+     * */
+    public int pathSum(TreeNode root, int sum) {
+
+    }
+
+
+
     public static void main(String[] args){
         TreeNode s = new TreeNode(3);
         s.left = new TreeNode(4);
